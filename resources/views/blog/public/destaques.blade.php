352699b1e76b8ffc9a01d7eba48c228db3ee2cbb
Marcio Nido <marcionido@gmail.com>
2017-03-01 22:05:45 -0500

    bugs

--- resources/views/blog/public/destaques.blade.php
+++ resources/views/blog/public/destaques.blade.php
@@ -20,7 +20,7 @@ $posts = \App\Blog\Post::getDestaques();
 
             <div class="thumbnail">
                 <a href="/blogleardi/{{$post->key}}">
-                    <img class="img-responsive" style="width: 100%" src="{{ $post->imagem->arquivo }}" />
+                    <img class="img-responsive" style="width: 100%" src="/wp-content/uploads/{{ $post->imagem->arquivo }}" />
                 </a>
                 <div class="caption">
                     <h3><a href="/blogleardi/{{$post->key}}">{{ $post->titulo }}</a></h3>
