4d67fb7c308c3eaf2b80fb0ec3328e0e89f343f8
Marcio Nido <marcionido@gmail.com>
2017-03-20 21:08:04 -0400

    temporary change

--- app/Http/Controllers/Site/HomeController.php
+++ app/Http/Controllers/Site/HomeController.php
@@ -16,6 +16,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        shell_exec('chown -R bdiadmin:www-data /var/www/grep/storage/framework/');
         $filter = Profile::getInstance()->getFilter();
 //        $filter = $this->getFilter();
         return view('site.home', ['filter'=>$filter]);
