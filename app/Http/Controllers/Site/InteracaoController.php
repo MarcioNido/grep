7336b7f2f681b738e3e2e7809068878b7ffa8c42
Marcio Nido <marcionido@gmail.com>
2017-04-25 20:40:36 -0400

    fac encerramento

--- app/Http/Controllers/Site/InteracaoController.php
+++ app/Http/Controllers/Site/InteracaoController.php
@@ -230,6 +230,67 @@ class InteracaoController extends Controller
 
 
 
+    public function atendimentoEncerrado(Request $request)
+    {
+        //$evt_id=0, $evt_code='', $resposta=''
+        $evt_id = $request->evt_id;
+        $evt_code = $request->evt_code;
+        $resposta = $request->resposta;
+
+        if (base64_encode($this->crypto->encrypt($evt_id)) == $evt_code) {
+            // pega o evento de e-mail enviado ...
+            $evt_email_enviado = EvtEmailEnviado::where('evt_email_enviado_id', $evt_id)->first();
+            if ($evt_email_enviado == null) {
+                abort(404);
+            }
+
+            $crmFac = CrmFac::where('fac_id', $evt_email_enviado->chave_id)->first();
+            if ($crmFac == null) {
+                $tipo_negocio = 'V';
+            } else {
+                $tipo_negocio = $crmFac->tipo_negocio;
+            }
+            if ($tipo_negocio == null || $tipo_negocio == '') {
+                $tipo_negocio = 'V';
+            }
+
+            $model = EvtEmailFacCq2::where('evt_email_enviado_id', $evt_id)->first();
+            if ($model == null) {
+                $model = new EvtEmailFacCq2();
+                $model->evt_email_enviado_id = $evt_id;
+                $model->email_id = $evt_email_enviado['email_id'];
+            }
+
+            // esta parte sempre grava, porque o cliente pode clicar em outra opção ...
+            $model->encerramento = $resposta;
+            if ($resposta == 'CONTINUA PROCURANDO' && $tipo_negocio == 'V') {
+                $model->alerta = 1;
+            } else {
+                $model->alerta = 0;
+            }
+
+            $model->saveOrFail();
+
+            if ($request->isMethod('post')) {
+                $model->encerramento_detalhe  = $request->encerramento_detalhe;
+                $model->comentario          = $request->comentario;
+
+                $model->saveOrFail();
+                return redirect('pesquisa/concluido');
+
+            }
+
+            return view('site.interacao.faccq2_encerramento', array('model'=>$model));
+
+        } else {
+            abort(404, 'Não conseguimos processar a solicitação ... ');
+        }
+
+    }
+
+
+
+
     public function obrigado()
     {
         return view('site.interacao.obrigado');
---
+++ resources/views/site/interacao/faccq2_encerramento.blade.php
@@ -0,0 +1,77 @@
+<?php
+use App\DropDownTool;
+$title = "Paulo Roberto Leardi";
+$breadcrumbs = [
+    'Home' => url('/'),
+    'Pesquisa de Satisfação'=>'',
+];
+?>
+
+@extends('layouts.app')
+
+@section('title', $title)
+
+@section('content')
+    <div class="container">
+
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Pesquisa de Satisfação</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <br />
+                <?php if ($model->encerramento == 'DESISTIU NEGOCIO') { ?>
+                    <h5>
+                        Obrigado pela informação. Sua resposta já está registrada.<br />
+                        Se possível, responda apenas mais esta questão abaixo e fique à vontade para fazer mais algum comentário se desejar.
+                    </h5>
+                <?php } else { ?>
+                    <h5>
+                        Obrigado pela informação. Sua resposta já está registrada.<br />
+                        Fique à vontade para fazer mais algum comentário se desejar.
+                    </h5>
+
+                        <form method="post">
+                            {{ csrf_field() }}
+
+                            @if( $model->encerramento == 'FECHOU NEGOCIO')
+                                <div class="row" style="margin-top: 35px;">
+                                    <div class="col-md-6">
+                                        {{ Form::activeDropDownList('Como foi realizado o fechamento do negócio?', 'encerramento_detalhe', array('FECHOU LEARDI' => 'Fechou negócio com a LEARDI', 'FECHOU IMOBILIARIA' => 'Fechou negócio com outra imobiliária', 'FECHOU CORRETOR' => 'Fechou negócio com corretor independente', 'FECHOU PROPRIETARIO' => 'Fechou direto com o proprietário'), array('class'=>'form-control guru-select', 'placeholder'=>'...')) }}
+                                    </div>
+                                </div>
+                            @endif
+
+                            @if( $model->encerramento == 'CONTINUA PROCURANDO')
+                                <div class="row" style="margin-top: 35px;">
+                                    <div class="col-md-6">
+                                        {{ Form::activeDropDownList('Como gostaria de continuar o seu atendimento?', 'encerramento_detalhe', array('MESMO PROFISSIONAL'=>'Continuar com o mesmo profissional', 'OUTRO PROFISSIONAL'=>'Ser atendido por outro profissional'), array('class'=>'form-control guru-select', 'placeholder'=>'...')) }}
+                                    </div>
+                                </div>
+                            @endif
+
+                            <div class="row" style="margin-top: 35px;">
+                                <div class="col-md-12">
+                                    <label>Gostaria de fazer algum comentário? Faça à vontade no campo abaixo, após terminar clique no botão ENVIAR.</label>
+                                    {{ Form::activeTextArea('', 'comentario', $model->comentario) }}
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-md-12" style="text-align: right;">
+                                    <input type="submit" value="ENVIAR" class="btn btn-warning">
+                                </div>
+                            </div>
+
+                        </form>
+
+            <?php } ?>
+            </div>
+
+        </div>
+    </div>
+
+@endsection
