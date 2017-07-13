041271c41ffde0b8557f36409839b47b3d1bdc5a
Marcio Nido <marcionido@gmail.com>
2017-07-12 20:24:44 -0400

    footer css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -106,7 +106,10 @@
 
     <footer class="footer" style="background-color: #345C8C;">
         <div class="container">
-            <p class="pull-left" style="color: #FFFFFF;">&copy; Paulo Roberto Leardi <?= date('Y') ?><span style="color: #BBBBBB"> - As informações aqui constantes são fornecidas pelo proprietário do imóvel e estão sujeitas a alteração a qualquer momento.</span></p>
+            <p class="pull-left" style="color: #FFFFFF;">&copy; Paulo Roberto Leardi <?= date('Y') ?>
+                <span style="color: #BBBBBB"> - As informações aqui constantes são fornecidas pelo proprietário do imóvel e estão sujeitas a alteração a qualquer momento.</span>
+                <span style="color: #BBBBBB"> - Cada unidade é juridica e financeiramente independente e registrada no Creci.</span>
+            </p>
             <!--
             <p class="pull-right"><a href="#" style="color: #E7E7E7;"><?= "by <b>GREP</b>" ?></a></p>
             -->
